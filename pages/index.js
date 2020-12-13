da08ddb2903d69306d18755fbb050967feeb451e
hoshi4444 <hoshi44444444@gmail.com>
2020-12-13 21:07:59 +0900

    lean getdata

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,47 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+
+export async function getStaticProps() {
+  // const allPostsData = await getSortedPostsData() fetch用
+  const allPostsData =  getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData} ) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>クソニート</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+            {/* fetch用
+            <li className={utilStyles.listItem} key={allPostsData.query.pages["128948"].id}>
+              {allPostsData.query.pages["128948"].title}
+              <br />
+              {allPostsData.query.pages["128948"].pageid}
+              <br />
+              {allPostsData.query.pages["128948"].touched}
+            </li> */}
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
